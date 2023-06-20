3993c7093649ecac5c3ad22c2819a530df6393cd
DeAndre Hill <deandre.hill2001@gmail.com>
2023-06-19 21:12:50 -0500

    added promises for tags

---
+++ utils/data/tagData.js
@@ -0,0 +1,64 @@
+import { clientCredentials } from '../client';
+
+const getTags = () => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/tags`)
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// eslint-disable-next-line import/prefer-default-export
+
+const createTag = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/tags`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getSingleTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/tags/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    }, // you technically do not need the options object for GET requests, but using it here for consistency
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data)) // will resolve a single object
+    .catch(reject);
+});
+
+const deleteSingleTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/tags/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTag = (payload) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/tags/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getTags, createTag, getSingleTag, updateTag, deleteSingleTag,
+};
