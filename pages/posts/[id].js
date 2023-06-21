bddcd0ab6587ad9fdf1814ac29b8ceeaa2fb6ad8
Erin Stephens <erebstephens@gmail.com>
2023-06-20 21:57:20 -0500

    added button to view post page to redirect to post comments

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -15,6 +15,10 @@ export default function ViewPost() {
     getSinglePost(id).then(setPostDetails);
   }, [id]);
 
+  const handleClick = () => {
+    router.push(`/posts/${id}/comments`);
+  };
+
   return (
     <>
       <Head>
@@ -33,6 +37,7 @@ export default function ViewPost() {
           <p className="PD-desc">Posted on: {postDetails.publication_date || ''}
           </p>
           <hr />
+          <Button variant="outline-dark" className="m-2" onClick={handleClick}>View Comments</Button>
           <Link passHref href="/posts">
             <Button variant="outline-dark" className="m-2">Return To Posts</Button>
           </Link>
