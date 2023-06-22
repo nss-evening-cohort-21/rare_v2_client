dd9ee89f642f318d786880dde2c8fd8ad285e4aa
ryanmbigelow <ryanmbigelow@gmail.com>
2023-06-22 18:24:10 -0500

    checking out commentsbypost-ers

--- components/post/PostForm.js
+++ components/post/PostForm.js
@@ -1,51 +1,51 @@
-import { useRouter } from 'next/router';
-import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { createEvent, createPost } from '../../utils/data/postData';
+// import { useRouter } from 'next/router';
+// import PropTypes from 'prop-types';
+// import { useState, useEffect } from 'react';
+// import { Button, Form } from 'react-bootstrap';
+// import { createEvent, createPost } from '../../utils/data/postData';
 
-const initialState = {
-  rareUserId: 0,
-  categoryId: 0,
-  title: '',
-  imageUrl: '',
-};
+// const initialState = {
+//   rareUserId: 0,
+//   categoryId: 0,
+//   title: '',
+//   imageUrl: '',
+// };
 
-const PostForm = ({ user, postObj }) => {
-  const [currentPost, setCurrentPost] = useState(initialState);
-  const router = useRouter();
+// const PostForm = ({ user, postObj }) => {
+//   const [currentPost, setCurrentPost] = useState(initialState);
+//   const router = useRouter();
 
-  useEffect(() => {
-    if (postObj.id) setCurrentPost(postObj);
-  }, [postObj]);
+//   useEffect(() => {
+//     if (postObj.id) setCurrentPost(postObj);
+//   }, [postObj]);
 
-  const handleChange = (e) => {
-    // TODO: Complete the onChange function
-    const { name, value } = e.target;
-    setCurrentPost((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
+//   const handleChange = (e) => {
+//     // TODO: Complete the onChange function
+//     const { name, value } = e.target;
+//     setCurrentPost((prevState) => ({
+//       ...prevState,
+//       [name]: value,
+//     }));
+//   };
 
-  const handleSubmit = (e) => {
-    // Prevent form from being submitted
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     // Prevent form from being submitted
+//     e.preventDefault();
 
-    const post = {
-      rareUserId: Number(postObj.rareUserId),
-      categoryId: Number(postObj.categoryId),
-      title: postObj.title,
-      publicationDate: postObj.publicationDate,
-      imageUrl: postObj.imageUrl,
-      content: postObj.content,
-      approved: Boolean(postObj.approved),
-    };
+//     const post = {
+//       rareUserId: Number(postObj.rareUserId),
+//       categoryId: Number(postObj.categoryId),
+//       title: postObj.title,
+//       publicationDate: postObj.publicationDate,
+//       imageUrl: postObj.imageUrl,
+//       content: postObj.content,
+//       approved: Boolean(postObj.approved),
+//     };
 
-    if (postObj.id) {
-      console.warn(postObj);
-    } else {
-      createPost(post).then(() => router.push('/posts'));
-    }
-  };
-};
+//     if (postObj.id) {
+//       console.warn(postObj);
+//     } else {
+//       createPost(post).then(() => router.push('/posts'));
+//     }
+//   };
+// };
