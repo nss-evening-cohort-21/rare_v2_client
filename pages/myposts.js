c2b1cca217658080f184383a775cc1ca0e288740
ryanmbigelow <ryanmbigelow@gmail.com>
2023-07-08 09:45:08 -0500

    merged main into view post image

--- pages/myposts.js
+++ pages/myposts.js
@@ -1,18 +1,21 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import PostCard from '../../components/post/PostCard';
-import { getPosts } from '../../utils/data/postData';
+import PostCard from '../components/post/PostCard';
+import { useAuth } from '../utils/context/authContext';
+import { getPostsByUserId } from '../utils/data/postData';
 
 function Home() {
   const router = useRouter();
+  const { user } = useAuth();
   const [posts, setPosts] = useState([]);
-  const getAllPosts = () => {
-    getPosts().then((data) => setPosts(data));
+
+  const getAllUserPosts = () => {
+    getPostsByUserId(user.id).then((data) => setPosts(data));
   };
 
   useEffect(() => {
-    getAllPosts();
+    getAllUserPosts();
   }, []);
 
   return (
@@ -25,12 +28,12 @@ function Home() {
         Create New Post
       </Button>
       <article className="posts">
-        <h3>Posts</h3>
+        <h3>My Posts</h3>
         {posts.map((post) => (
           <section key={`post--${post.id}`} className="posts">
-            <PostCard postObj={post} onUpdate={getAllPosts} />
+            <PostCard postObj={post} />
           </section>
-        ))}
+        ))};
       </article>
     </>
   );
